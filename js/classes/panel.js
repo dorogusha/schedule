b0debdd6fa1b5dd53ebb4d38ed074ca6ff3fe489
Tatsiana Vasiukovich <tava@gurtam.com>
2017-05-30 16:55:29 +0300

    new files

--- js/classes/panel.js
+++ js/classes/panel.js
@@ -1,17 +1,43 @@
 VHV.Panel = function(settings){
+	VHV.Panel.base.constructor.apply(this, settings);
 
-	VHV.Panel.prototype.constructor.call(this, settings);
-	//VHV.initGlobalProperties(this, ["Title"]);
+	this.domNode = document.createElement("div");
+	this.domNode.className = "VHV-Panel";
+
+	this.setContent();
+
+	this.updateSize();
 };
 
-VHV.initClass("Panel", "DomElement");
-
+VHV.initClass(VHV.Panel, VHV.Ui);
+VHV._initProperties(VHV.Ui, {
+	"title": {get: true}
+});
 
 var panel = VHV.Panel.prototype;
 
+panel.setContent = function(value){
+	if (this.contentNode) {
+		value = value + "";
+		$(this.contentNode).html(value);
+		this.content = value;
+	} else {
+
+		VHV.Panel.base.setContent.apply(this, arguments);
+
+		this.titleNode = this.domNode.firstChild;
+		this.contentNode = this.domNode.lastChild;
+	}
+};
+
+panel.setTitle = function(value){
+	value = value + "";
+	$(this.titleNode).html(value);
+	this.title = value;
+};
+
 panel.onShow = function(){
-	var node = this.getDomNode();
-	console.log(node);
-	VHV.Panel.prototype.onShow.call(this, settings);
+	VHV.Panel.base.onShow.apply(this, arguments);
+
 };
 panel = null;
--- js/core.js
+++ js/core.js
@@ -26,10 +26,11 @@ VHV.isArray = function(val){
 	return (val && typeof val == "object" && Array.isArray(val));
 };
 
-VHV.initClass = function(className, prototypeName, mixed) {
-	var cls = VHV[className],
-		proto = VHV[prototypeName];
+VHV.isString = function(val){
+	return (val && typeof val == "string");
+};
 
+VHV.initClass = function(cls, proto, mixed) {
 	if (!cls)
 		new VHV.Error (VHV.vocabulary.initClass_1);
 
@@ -40,7 +41,8 @@ VHV.initClass = function(className, prototypeName, mixed) {
 		F.prototype = proto.prototype;
 		cls.prototype = new F();
 		cls.prototype.constructor = cls;
-		cls.superclass = proto.prototype;
+		cls.base = proto.prototype;
+		cls.baseClass = proto;
 	}
 
 	mixed.forEach(function(m){
@@ -50,64 +52,40 @@ VHV.initClass = function(className, prototypeName, mixed) {
 				cls.prototype[mProperty] = mixedData[mProperty];
 		}
 	});
-
-	VHV.nameSpaces[className] = {proto: prototypeName, mixed: mixed || []};
 }
 
-VHV.initPrivateProperties = function(cls, properties, defaultValues){
-	if (!VHV.isObject(cls) || !VHV.isArray(properties))
+VHV._initProperties = VHV.initProperties = function(cls, properties){
+	if (!cls || !cls.prototype || !VHV.isObject(properties))
 		return;
-	for (var i = 0, l = properties.length; i < l; i++) {
-		var prop = properties[i],
-			defaultVal = defaultValues ? defaultValues[prop] : undefined;
-		cls[prop] = defaultVal;
+	for (var p in properties) {
+		var prop = properties[p],
+			defaultValue = prop.value,
+			validPropName = p[0].toUpperCase() + p.substr(1);
+
+		cls.prototype[prop] = defaultValue;
+
+		if (prop.get)
+			cls.prototype["get" + validPropName] = function(){
+				return this[validPropName];
+			};
+
+		if (prop.set)
+			cls.prototype["set" + validPropName] = function(value){
+				this[validPropName] = value;
+			};
 	}
 };
 
-VHV.initGlobalProperties = VHV.initProperties = function(className, properties, defaultValues){
-	var cls = VHV[className];
-	if (!cls || !VHV.isArray(properties))
-		return;
-	for (var i = 0, l = properties.length; i < l; i++) {
-		var prop = properties[i],
-			defaultVal = defaultValues ? defaultValues[prop] : undefined;
-		cls.prototype[prop] = defaultVal;
-	}
-};
-
-VHV.initGetter = function(className, properties){
-	var cls = VHV[className];
-	if (!cls || !VHV.isArray(properties))
-		return;
-
-	for (var i = 0, l = properties.length; i < l; i++) {
-		var prop = properties[i],
-			validPropName = prop[0].toUpperCase() + prop.substr(1);
-		cls.prototype["get" + validPropName] = function(){
-			return this[validPropName];
-		};
-	}
-};
-
-VHV.initSetter = function(className, properties){
-	var cls = VHV[className];
-	if (!cls || !VHV.isArray(properties))
-		return;
-
-	for (var i = 0, l = properties.length; i < l; i++) {
-		var prop = properties[i],
-			validPropName = prop[0].toUpperCase() + prop.substr(1);
-		cls.prototype["set" + validPropName] = function(value){
-			this[validPropName] = value;
-		};
-	}
-};
+VHV.templates = {
+	"panel": "<div class='VHV-Panel-Title'></div>" +
+			 "<div class='VHV-Panel-Content'></div>"
+}
 
 // Базовые классы
 VHV.Base = function(){
 
 };
-VHV.initClass("Base");
+VHV.initClass(VHV.Base);
 var bP = VHV.Base.prototype;
 
 bP.destroy = function(){
@@ -120,7 +98,7 @@ bP.destroy = function(){
 };
 bP = null;
 
-VHV.DomElement = function(settings) {
+VHV.Ui = function(settings) {
 	if (settings) {
 		if (settings.size)
 			this.size = settings.size;
@@ -128,18 +106,44 @@ VHV.DomElement = function(settings) {
 			this.template = settings.template;
 	}
 
-	VHV.DomElement.prototype.constructor.call(this, settings);
+	VHV.Ui.base.constructor.apply(this, arguments);
 };
 
-VHV.initClass("DomElement", "Base");
-VHV.initGlobalProperties("DomElement", ["domNode", "Content", "Size"]);
-VHV.initGetter("DomElement", ["domNode", "Content", "Size"]);
-VHV.initSetter("DomElement", ["domNode", "Content", "Size"]);
+VHV.initClass(VHV.Ui, VHV.Base);
+VHV._initProperties(VHV.Ui, {
+	"domNode": {get: true, set: true},
+	"content": {get: true},
+	"size": {get: true, set: true, value: {w: "auto", h: "auto"}}
+});
 
-var dEl = VHV.DomElement.prototype;
+var dEl = VHV.Ui.prototype;
+
+dEl.setContent = function(value){
+	if (this.domNode){
+		if (VHV.isString(value))
+			this.domNode.innerHTML = value || "";
+		else if (value.innerHTML)
+			this.domNode.appendChild(value);
+	}
+};
+
+dEl.setWidth = function(value){
+	$(this.domNode).width(value);
+	this.size.w = $(this.domNode).width();
+};
+
+dEl.setHeight = function(value){
+	$(this.domNode).height(value);
+	this.size.h = $(this.domNode).height();
+};
+
+dEl.updateSize = function(){
+	this.setWidth(this.size.w);
+	this.setHeight(this.size.h);
+};
 
 dEl.onShow = function(){
-	documentt.body.appendChild(this.domNode);
+	document.body.appendChild(this.domNode);
 };
 
 dEl = null;
